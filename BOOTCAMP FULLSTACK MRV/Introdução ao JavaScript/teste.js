c960673189a913f86df5484be298467e34e227a4
rafaellessacyber <rafaellessadossantos.rf@gmail.com>
2022-01-21 21:57:50 -0300

    Execução do JavaScript com node

---
+++ BOOTCAMP FULLSTACK MRV/Introdução ao JavaScript/teste.js
@@ -0,0 +1,20 @@
+ //Abaixo é o comentário de linha
+ //var y = 0;
+ 
+
+ /*Abaixo é comentário de várias linhas ao qual inicia-se com barra asterisco, e termina com o asterisco barra para terminar o comentário de várias linhas*/
+
+ /*var y = 0;
+ var x = 1; */
+
+ //var preco = 2;
+ var desconto = 0.2;
+ const PRECO = 2;
+ 
+//Operação válida, pois ela tem uma variável total tornando possível a manipulação de variação de dados. Ou seja, não sendo possível manipular uma constante nessa seguinte expressão lógica, que é: PRECO = PRECO - desconto. Lembrando de que se trata de uma constante ao qual os valores não podem serem manipulados.*/
+
+ var total = PRECO - desconto
+
+ 
+/*Operação válida quando for somente uma variável
+ preco = preco - desconto*/
---
+++ BOOTCAMP FULLSTACK MRV/Introdução ao JavaScript/teste2.js
@@ -0,0 +1,5 @@
+fuction soma (a, b) {
+  return a + b; //8
+}
+
+soma (3 ,5);
